8ee88af1264ab95c4a6a46f77bf98c326fe99664
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-09-11 02:37:21 -0300

    wip

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-# import datetime
+import datetime
 
-# import uvicorn
-# from fastapi import FastAPI
+import fastapi
+import uvicorn
 
-# # from auth import router as auth_router
+from router import router
 
-# app = FastAPI()
+HOST = "0.0.0.0"
+PORT = 8081
 
-
-# @app.get("/")
-# async def root() -> list[datetime.datetime]:
-#     dummy_times = [
-#         datetime.datetime(2018, 1, 1, 10, 0, 0),
-#         datetime.datetime(2018, 1, 2, 10, 30, 0),
-#         datetime.datetime(2018, 1, 3, 11, 0, 0),
-#     ]
-
-#     return dummy_times
+app = fastapi.FastAPI()
+app.include_router(router)
 
 
-# app.include_router(auth_router, prefix="/auth")
+@app.get("/")
+async def root() -> list[datetime.datetime]:
+    dummy_times = [
+        datetime.datetime(2018, 1, 1, 10, 0, 0),
+        datetime.datetime(2018, 1, 2, 10, 30, 0),
+        datetime.datetime(2018, 1, 3, 11, 0, 0),
+    ]
+
+    return dummy_times
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=8080, workers=1)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=HOST, port=PORT, workers=1)

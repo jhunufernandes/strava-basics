598200de0fbb4d2ba7469f2517946733b7c9e86c
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-09-17 15:54:26 -0300

    wip

--- auth.py
+++ auth.py
@@ -1,17 +1,24 @@
 import typing
-import uvicorn
 
 import fastapi
 import fastapi.security
+import uvicorn
 
+from router import router
 
 HOST = "0.0.0.0"
 PORT = 8080
 
+app = fastapi.FastAPI()
+app.include_router(router)
+
 
 oauth2_authorization_code_bearer = fastapi.security.OAuth2AuthorizationCodeBearer(
-    authorizationUrl="https://strava.com/oauth2/authorize",
-    tokenUrl="https://strava.com/oauth2/token",
+    # authorizationUrl="https://strava.com/oauth2/authorize",
+    # tokenUrl="https://strava.com/oauth2/token",
+    authorizationUrl="http://localhost:8081/authorize",
+    tokenUrl="http://localhost:8081/token",
+    scopes={"read": "Read-only access", "activity:read_all": "Read all activities"},
 )
 
 
@@ -25,13 +32,10 @@ async def get_token(
     return await oauth2_scheme(request)
 
 
-app = fastapi.FastAPI()
-
-
 @app.get("/")
 async def root(token: typing.Annotated[typing.Any, fastapi.Security(get_token)]):
     return {"token": token}
 
 
 if __name__ == "__main__":
-    uvicorn.run("auth:app", host=HOST, port=PORT, workers=1)
+    uvicorn.run("auth:app", host=HOST, port=PORT, workers=1, reload=True)
